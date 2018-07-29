64b8aec4cde12a1cf4afc5b80ad6a70960713f03
Temeka <misstemeka1@live.com>
2018-07-29 16:56:48 -0500

    Add project files.

---
+++ WeekFourArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekFourArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
