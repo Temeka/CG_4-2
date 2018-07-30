2d5610fc7bbaa0a3b1db7ef8cc650884e4415cb3
Temeka <misstemeka1@live.com>
2018-07-30 18:56:11 -0500

    Looping Arrays

--- WeekFourArray/Program.cs
+++ WeekFourArray/Program.cs
@@ -6,7 +6,21 @@ namespace WeekFourArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creating and initializing the "Mekarrays" and the values
+            int[] Mekarrays = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            //Looping using 'foreach' vs 'for' to create a single line
+            foreach (int digits in Mekarrays)
+             {
+                //Telling compiler to write the #s
+                //with a space inbetween each value
+                Console.Write("{0} ", digits);
+             }
+             Console.ReadLine();
+             
+
+            
         }
+
     }
 }
